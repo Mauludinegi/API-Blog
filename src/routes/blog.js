65d3e8b2a52e96cb49f5106920d344fdfa451413
Egi Purnama M <111052101+Mauludinegi@users.noreply.github.com>
2022-12-07 14:58:10 +0700

    Add files via upload

---
+++ src/routes/blog.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const {body} = require('express-validator');
+
+const router = express.Router();
+const blogController = require('../controllers/blog');
+
+
+router.post('/post', [
+        body('title').isLength({min: 5}).withMessage('input title minimal 5 karakter'),
+        body('body').isLength({min: 5}).withMessage('input body minimal 5 karakter')],
+        blogController.createBlogPost); 
+
+router.get('/posts', blogController.getAllBlogPost)
+
+router.get('/post/:postId', blogController.getBlogPostById)
+
+router.put('/post/:postId', [
+    body('title').isLength({min: 5}).withMessage('input title minimal 5 karakter'),
+    body('body').isLength({min: 5}).withMessage('input body minimal 5 karakter')],
+    blogController.updateBlogPost)
+
+router.delete('/post/:postId', blogController.deleteBlogPost);
+
+module.exports = router;
